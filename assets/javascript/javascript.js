d02cdc5e50f41c13789b7a56a6db0d3b53043acd
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-06-27 22:14:15 -0700

    added some stylings

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,35 @@
+/*
+    TODO: LInk and configure firebase
+*/
+
+/*
+    TODO: Create object for times
+        -new object pushed inside the TImes obj
+*/
+
+let TrainTimes = [
+    {
+        trainName: "Trenton Express",
+        destination: "Treton",
+        Frequency: 25,
+        nextArrival: 0,
+        minutesAway: 0
+    }
+]
+
+$(document).ready(function() {
+    var a = moment().format('HH')
+    console.log(a);
+})
+
+/*
+    TODO: function to update nextArrivalTimes
+*/
+
+/*
+    TODO: function to update minutesAway
+*/
+
+/*
+    TODO: function to update display with the times
+*/
