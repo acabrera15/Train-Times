e1c3ae5d07afb7fabeecb6d88820c2f973258098
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-07-03 21:27:18 -0700

    now error checks"

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,51 +3,93 @@
 */
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyCafaCMpaYr7QZeBSHu3V5yDUibyHqBqOM",
-    authDomain: "trainsproj.firebaseapp.com",
-    databaseURL: "https://trainsproj.firebaseio.com",
-    projectId: "trainsproj",
-    storageBucket: "",
-    messagingSenderId: "67900234466",
-    appId: "1:67900234466:web:98853b97de0ffb73"
-  };
+  apiKey: "AIzaSyCafaCMpaYr7QZeBSHu3V5yDUibyHqBqOM",
+  authDomain: "trainsproj.firebaseapp.com",
+  databaseURL: "https://trainsproj.firebaseio.com",
+  projectId: "trainsproj",
+  storageBucket: "",
+  messagingSenderId: "67900234466",
+  appId: "1:67900234466:web:98853b97de0ffb73"
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
-
 $(document).ready(function() {
-  var a = moment().format("HH");
-  console.log(a);
+  calculateNextArrivalTime("sd");
 });
 
-$("#submit").on('click', function(event) {
-    event.preventDefault();
+$("#submit").on("click", function(event) {
+  event.preventDefault();
 
-    let trainName = $('#trainNameInput').val().trim();
-    let destination = $('#destinationInput').val().trim();
-    let firstTrainTime = $('#firstTrainTimeInput').val().trim();
-    let frequency = $('#frequencyInput').val().trim();
-    console.log(trainName, destination, firstTrainTime, frequency)
+  $('#trainTimeError').empty();
+  $("#frequencyError").empty();
+  let frequency = $("#frequencyInput")
+    .val()
+    .trim();
+
+  if (!isNaN(frequency)) {
+    let firstTrainTime = $("#firstTrainTimeInput")
+    .val()
+    .trim();
+    console.log(firstTrainTime);
+
+    if (moment(firstTrainTime, "HH:mm").isValid()) {
+      let trainName = $("#trainNameInput")
+      .val()
+      .trim();
+    let destination = $("#destinationInput")
+      .val()
+      .trim();
+
+
+    console.log(trainName, destination, firstTrainTime, frequency);
     database.ref().push({
-        trainName,
-        destination,
-        firstTrainTime,
-        frequency
-    })
-})
+      trainName,
+      destination,
+      firstTrainTime,
+      frequency
+    });
+    } else {
+      $(
+        "<p class='text-danger' id='trainTimeError'>Error: please type in a valid time!</p>"
+      ).appendTo("#trainTimeDiv");
+    }
 
-database.ref().on('child_added', function(snapshot) {
+  } else {
+    $(
+      "<p class='text-danger' id='frequencyError'>Error: please type in a valid number!</p>"
+    ).appendTo("#frequencyDiv");
+  }
+});
 
-    $("#trainSchedules").append(`<tr>
+database.ref().on("child_added", function(snapshot) {
+  const minutesUntilArrival = calculateMinutesToNextArrivalTime(
+    moment(snapshot.val().firstTrainTime, "HH:mm"),
+    snapshot.val().frequency
+  );
+  const nextArrivalTime = calculateNextArrivalTime(minutesUntilArrival);
+
+  $("#trainSchedules").append(`<tr>
     <td>${snapshot.val().trainName}</td>
     <td>${snapshot.val().destination}</td>
     <td>${snapshot.val().frequency}</td>
-    <td>${snapshot.val().firstTrainTime}</td>
-    <td>${'test'}</td>
+    <td>${nextArrivalTime}</td>
+    <td>${minutesUntilArrival}</td>
   </tr>`);
-})
+});
 
-let calculateNextArrivalTime = function() {
-    
-}
+let calculateMinutesToNextArrivalTime = function(
+  trainTimeStartTime,
+  frequency
+) {
+  const currentTime = moment();
+  var diff = currentTime.diff(trainTimeStartTime, "minutes") % frequency;
+  return frequency - diff;
+};
+
+let calculateNextArrivalTime = function(minutesAway) {
+  currentTime = moment();
+
+  return currentTime.add(minutesAway, "minutes").format("h:mm A");
+};
